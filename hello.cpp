2b3f710cd548c00906583bfaab10c1ae9ce11ad1
Mohammad Khan <khanmo1726@gmail.com>
2018-06-25 17:16:15 -0400

    takes in starting cells as input

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <string>
+#include <sstream>
 using namespace std;
 
 class GameContents {
@@ -14,8 +16,8 @@ public:
 
 GameContents::GameContents(int s, vector<pair<int, int>> starting_cells) {
     size = s;
-    vector<vector<char>> initial_board(size, vector<char>(size, '+'));
-    for (int i = 0; i < starting_cells.size(); ++i) {
+    vector<vector<char>> initial_board(size, vector<char>(size, '.'));
+    for (size_t i = 0; i < starting_cells.size(); ++i) {
         int row = starting_cells[i].first;
         int col = starting_cells[i].second;
         initial_board[row][col] = 'o';
@@ -32,10 +34,36 @@ void GameContents::printBoard() {
     }
 }
 
+pair<int, int> readPair(string coordinate) {
+    vector<int> coordinate_vec;
+    pair<int, int> coordinate_pair;
+    istringstream iss(coordinate);
+    for (int i = 0; i < 2; ++i) {
+        string str;
+        int num;
+        iss >> str;
+        if (str.back() == ',') {
+            str.pop_back();
+        }
+        num = stoi(str);
+        coordinate_vec.push_back(num);
+    }
+    coordinate_pair.first = coordinate_vec[0];
+    coordinate_pair.second = coordinate_vec[1];
+    return coordinate_pair;
+}
+
 int main() {
-    pair<int, int> pos1(1, 2);
-    pair<int, int> pos2(1, 3);
-    vector<pair<int, int>> starting_cells = { pos1, pos2 };
+    string coordinate;
+    vector<pair<int, int>> starting_cells;
+    cout << "Please enter you starting positions in the form x, y (with a space after the comma), each followed by an ENTER:\n";
+    cout << "When you are finished, hit ENTER again\n";
+    while (getline(cin, coordinate)) {
+        if (coordinate == "") {
+            break;
+        }
+        starting_cells.push_back(readPair(coordinate));
+    }
     GameContents mygame(10, starting_cells);
     mygame.printBoard();
 
