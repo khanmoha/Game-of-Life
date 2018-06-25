cd9015fed1b753d77bec690ded7e8bc9cec1ef60
Mohammad Khan <khanmo1726@gmail.com>
2018-06-25 02:22:32 -0400

    game board can be initialized

--- hello.cpp
+++ hello.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 using namespace std;
 
 class GameContents {
     int size; 
     vector<vector<char>> board;
-    vector<pair<int, int>> starting_cells;
+    //vector<pair<int, int>> starting_cells;
 public:
-    GameContents(int s);
-    void initializeBoard();
+    GameContents(int s, vector<pair<int, int>> starting_cells);
     void printBoard();
 };
 
-GameContents::GameContents(int s) {
+GameContents::GameContents(int s, vector<pair<int, int>> starting_cells) {
     size = s;
-}
-
-void GameContents::initializeBoard() {
-    vector<vector<char>> initial_board(size, vector<char>('x'));
+    vector<vector<char>> initial_board(size, vector<char>(size, '+'));
+    for (int i = 0; i < starting_cells.size(); ++i) {
+        int row = starting_cells[i].first;
+        int col = starting_cells[i].second;
+        initial_board[row][col] = 'o';
+    }
     board = initial_board;
 }
 
-int main() {
-    GameContents mygame(10);
+void GameContents::printBoard() {
+    for (int row = 0; row < size; ++row) {
+        for (int col = 0; col < size; ++col) {
+            cout << board[row][col] << " ";
+        }
+        cout << endl;
+    }
+}
 
+int main() {
+    pair<int, int> pos1(1, 2);
+    pair<int, int> pos2(1, 3);
+    vector<pair<int, int>> starting_cells = { pos1, pos2 };
+    GameContents mygame(10, starting_cells);
+    mygame.printBoard();
 
 
     return 0;
